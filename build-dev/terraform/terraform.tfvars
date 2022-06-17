6ca6f2cdc4dfa7757001fb80a54187fc8b5ad4a6
Pablo Patino <pablo@bitovi.com>
2022-06-17 11:34:15 -0500

    Add network

--- build-dev/terraform/terraform.tfvars
+++ build-dev/terraform/terraform.tfvars
@@ -1,3 +1,16 @@
-# Contentful secrets
+# CONTENTFUL secrets
 secret_arn_contentful_access_token = "arn:aws:secretsmanager:us-east-2:368433847371:secret:ContentfulAccessToken-NIU3zI"
 secret_arn_contentful_space_id = "arn:aws:secretsmanager:us-east-2:368433847371:secret:ContentfulSpaceID-FXm2ED"
+
+# APP
+app_name        = "fast-react-static-renderer"
+app_environment = "build-dev"
+common_tags = {
+  OperationsRepo = "bitovi/fast-react-static-renderer-operations"
+  OperationsRepoEnvironment = "build-dev"
+}
+
+# NETWORK
+availability_zones = ["us-east-2a","us-east-2b"]
+public_subnets     = ["10.10.100.0/24", "10.10.101.0/24"]
+private_subnets    = ["10.10.0.0/24", "10.10.1.0/24"]
--- build-dev/terraform/variables.tf
+++ build-dev/terraform/variables.tf
@@ -1,8 +1,36 @@
-# Contentful secrets
+# CONTENTFUL secrets
 variable "secret_arn_contentful_access_token" {
   description = "ARN for the contentful access token"
 }
 
 variable "secret_arn_contentful_space_id" {
   description = "ARN for the contentful space id"
+}
+
+# APP
+variable "app_name" {
+  type        = string
+  description = "Application Name"
+}
+
+variable "app_environment" {
+  type        = string
+  description = "Application Environment"
+}
+
+variable "common_tags" {
+  description = "Common tags you want applied to all components."
+}
+
+# NETWORKING
+variable "availability_zones" {
+  description = "List of availability zones"
+}
+
+variable "public_subnets" {
+  description = "List of public subnets"
+}
+
+variable "private_subnets" {
+  description = "List of private subnets"
 }
