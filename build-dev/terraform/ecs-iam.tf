e67e331d07ea59dc5529a8528b025b6d19388fb6
Pablo Patino <pablo@bitovi.com>
2022-06-23 13:53:57 -0500

    Update ECS role permission region

--- build-dev/terraform/ecs-iam.tf
+++ build-dev/terraform/ecs-iam.tf
@@ -78,8 +78,8 @@ resource "aws_iam_policy" "ecs-runner" {
       ],
       "Effect": "Allow",
       "Resource": [
-        "arn:aws:ecs:us-west-2:368433847371:task-definition/fast-react-static-renderer-task:*",
-        "arn:aws:ecs:us-west-2:368433847371:task/fast-react-static-renderer-build-dev-cluster/*"
+        "arn:aws:ecs:us-east-2:368433847371:task-definition/fast-react-static-renderer-task:*",
+        "arn:aws:ecs:us-east-2:368433847371:task/fast-react-static-renderer-build-dev-cluster/*"
       ]
     },
     {
