a4bc71e0d45f1c694104385c92177107bd8299c4
Pablo Patino <pablo@bitovi.com>
2022-07-07 11:02:42 -0500

    fix before script syntax error

--- dev/terraform/bitops.before-deploy.d/s3-create-tf-state-bucket.sh
+++ dev/terraform/bitops.before-deploy.d/s3-create-tf-state-bucket.sh
@@ -5,3 +5,4 @@ if [ "$AWS_DEFAULT_REGION" == "us-east-1" ]; then
   aws s3api create-bucket --bucket $TF_STATE_BUCKET --region $AWS_DEFAULT_REGION || true
 else
   aws s3api create-bucket --bucket $TF_STATE_BUCKET --region $AWS_DEFAULT_REGION --create-bucket-configuration LocationConstraint=$AWS_DEFAULT_REGION || true
+fi
