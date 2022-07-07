bc5a2126fb5d033293d217d1d5cf7d27d5ace9c9
Pablo Patino <pablo@bitovi.com>
2022-07-07 11:18:49 -0500

    begin removing ECR

--- global-tools/terraform/ecr.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "bitovi" {
-  name                 = "fast-react-static-renderer-build-image"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
