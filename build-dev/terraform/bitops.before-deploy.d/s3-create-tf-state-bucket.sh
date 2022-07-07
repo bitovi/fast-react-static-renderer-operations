d27432dc0dcd24161f7f44c758c15b6774620fb2
Pablo Patino <pablo@bitovi.com>
2022-07-07 10:58:48 -0500

    Update bitops before deploy scripts

--- build-dev/terraform/bitops.before-deploy.d/s3-create-tf-state-bucket.sh
+++ build-dev/terraform/bitops.before-deploy.d/s3-create-tf-state-bucket.sh
@@ -1,2 +1,7 @@
 echo "Creating Bucket: $TF_STATE_BUCKET"
-aws s3api create-bucket --bucket $TF_STATE_BUCKET --region $AWS_DEFAULT_REGION --create-bucket-configuration LocationConstraint=$AWS_DEFAULT_REGION || true
+
+#!/bin/bash
+if [ "$AWS_DEFAULT_REGION" == "us-east-1" ]; then
+  aws s3api create-bucket --bucket $TF_STATE_BUCKET --region $AWS_DEFAULT_REGION || true
+else
+  aws s3api create-bucket --bucket $TF_STATE_BUCKET --region $AWS_DEFAULT_REGION --create-bucket-configuration LocationConstraint=$AWS_DEFAULT_REGION || true
