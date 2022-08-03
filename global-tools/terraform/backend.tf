147bc35d45a48acbe17557136745759e49ba5f01
Pablo Patino <pablo@bitovi.com>
2022-08-03 11:36:44 -0500

    New tf state for no domain env

--- global-tools/terraform/backend.tf
+++ global-tools/terraform/backend.tf
@@ -5,7 +5,8 @@ terraform {
   backend "s3" {
     region  = "us-east-2"
     bucket  = "fast-react-static-renderer-operations-global-tools-state"
-    key     = "fast-react-static-renderer-operations-global-tools"
+    # TODO: reset on merge to main
+    key     = "fast-react-static-renderer-operations-global-tools-nodom"
     encrypt = true #AES-256encryption
   }
 }
