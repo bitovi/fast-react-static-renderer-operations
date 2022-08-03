f519b9024671416973652ad3ebc02b4b85acab50
Pablo Patino <pablo@bitovi.com>
2022-08-03 16:48:46 -0500

    fix conflict

--- dev/terraform/cloudfront.tf
+++ dev/terraform/cloudfront.tf
@@ -1,7 +1,8 @@
 # Cloudfront S3 for redirect slashes
 # https://advancedweb.hu/how-to-use-cloudfront-functions-to-change-the-origin-request-path/
 resource "aws_cloudfront_function" "rewrite_uri_react" {
-  name    = "rewrite-request-react"
+  # TODO: reset on merge to main
+  name    = "rewrite-request-react-nodom"
   runtime = "cloudfront-js-1.0"
   publish = true
   code    = file("${path.module}/js-rewrite/cf-url-rewrite.js")
