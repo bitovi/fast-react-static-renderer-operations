7a40eee462aca3055ddfce51d8ba0ebb08f21266
Pablo Patino <pablo@bitovi.com>
2022-08-03 20:30:57 -0500

    Document optional and required Terraform variables

--- dev/terraform/variables.tf
+++ dev/terraform/variables.tf
@@ -1,28 +1,28 @@
 variable "bucket_name" {
   type        = string
-  description = "The name of the bucket without the www. prefix. Normally domain_name."
+  description = "[REQUIRED] The name of the bucket without the www. prefix. Normally domain_name."
 }
 
 variable "domain_name" {
   type        = string
-  description = "The domain name."
+  description = "[OPTIONAL] The domain name."
   default     = null
 }
 
 variable "subdomain_name" {
   type        = string
-  description = "Subdomain that will be used for hosted env"
+  description = "[OPTIONAL] Subdomain that will be used for hosted env"
   default     = null
 }
 
 variable "catalog_domain_name" {
   type        = string
-  description = "Subdomain that will be used for hosted env catalog"
+  description = "[OPTIONAL] Subdomain that will be used for hosted env catalog"
   default     = null
 }
 
 variable "hosted_zone_id" {
   type        = string
-  description = "Hostedzone ID created in global-tools for domain name"
+  description = "[REQUIRED] Hostedzone ID created in global-tools for domain name"
   default     = null
 }
