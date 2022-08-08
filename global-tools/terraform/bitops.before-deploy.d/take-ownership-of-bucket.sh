3e7c85214f9cf29527db29e92c5278bef39129c2
Mick McGrath <mick@bitovi.com>
2022-08-08 17:00:21 -0400

    init backend before attempting the import

--- global-tools/terraform/bitops.before-deploy.d/take-ownership-of-bucket.sh
+++ global-tools/terraform/bitops.before-deploy.d/take-ownership-of-bucket.sh
@@ -3,4 +3,9 @@
 BUCKET="fast-react-static-renderer-artifacts"
 echo "Taking ownership of bucket: $BUCKET"
 
+cd $TERRAFORM_ROOT
+terraform-0.13.2 init
 terraform-0.13.2 import aws_s3_bucket.s3_static_files $BUCKET
+cd -
+echo "exiting"
+exit 1
