6826f9084c054493d0a4c6095a426a9e9e22ac54
Mick McGrath <mick@bitovi.com>
2022-08-08 16:56:20 -0400

    specify tf version executable for bucket ownership

--- global-tools/terraform/bitops.before-deploy.d/take-ownership-of-bucket.sh
+++ global-tools/terraform/bitops.before-deploy.d/take-ownership-of-bucket.sh
@@ -3,4 +3,4 @@
 BUCKET="fast-react-static-renderer-artifacts"
 echo "Taking ownership of bucket: $BUCKET"
 
-terraform import aws_s3_bucket.s3_static_files $BUCKET
+terraform-0.13.2 import aws_s3_bucket.s3_static_files $BUCKET
