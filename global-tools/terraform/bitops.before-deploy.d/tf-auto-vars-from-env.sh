179b4ee12fed0a75b35156949e49d0bdb4e1b1af
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2022-08-11 12:36:02 -0400

    ensure TFVARS_AUTO_JSON is conditional (#6)

--- global-tools/terraform/bitops.before-deploy.d/tf-auto-vars-from-env.sh
+++ global-tools/terraform/bitops.before-deploy.d/tf-auto-vars-from-env.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo "Creating TF Auto vars from env var (TFVARS_AUTO_JSON): $TFVARS_AUTO_JSON"
-AUTO_VARS_FILE="$TERRAFORM_ROOT/vars.auto.tfvars.json"
-echo $TFVARS_AUTO_JSON > $AUTO_VARS_FILE
+if [ -n "$TFVARS_AUTO_JSON" ]; then
+  echo "Creating TF Auto vars from env var (TFVARS_AUTO_JSON): $TFVARS_AUTO_JSON"
+  AUTO_VARS_FILE="$TERRAFORM_ROOT/vars.auto.tfvars.json"
+  echo $TFVARS_AUTO_JSON > $AUTO_VARS_FILE
 
-echo "File contents for $AUTO_VARS_FILE"
-cat $AUTO_VARS_FILE
+  echo "File contents for $AUTO_VARS_FILE"
+  cat $AUTO_VARS_FILE
+fi
